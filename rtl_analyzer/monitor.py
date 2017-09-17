ae13d9aedb51df3941e796c57bfd293af6fb2988
ab1gale <328149598@qq.com>
2017-09-17 23:42:03 +0800

    ktsan

--- rtl_analyzer/monitor.py
+++ rtl_analyzer/monitor.py
@@ -8,6 +8,10 @@ class NewFileHandler(pyinotify.ProcessEvent):
 def WatchNewFile():
 	wm=pyinotify.WatchManager()
         handler=NewFileHandler()
+	notifier = pyinotify.Notifier(wm, default_proc_fun=handler)
+	wm.add_watch('../report_trace_log', pyinotify.IN_CREATe, rec=True, auto_add=True)
+	print "start monitoring rtl"
+	notifier.loop()
 
 if __name__ == '__main__':
 	WatchNewFile()
