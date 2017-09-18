77cc08bffe7f55ed2f763b2ddc1219a42a54c3a3
ab1gale <328149598@qq.com>
2017-09-18 11:49:00 +0800

    syzkaller_hdl

--- rtl_analyzer/syzkaller_hdl.py
+++ rtl_analyzer/syzkaller_hdl.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 def handle(full_path,relative_path):
-	print "syz called"
 	l=len("syzkaller/")
 	r_path=relative_path[l:]
 	if r_path.startswith("crashes"):
-		syzkaller_crash_hdl(full_path)
-def syzkaller_crash_hdl(full_path):
-	print 'xxx'
+		crash_log(full_path,r_path)
+def crash_log(full_path,r_path):
+	clen=len("crashes/")
+	rp=r_path[clen:]
+	if rp.startswith("log"):
+		ordinary_log()
+	elif rp.startswith("repro"):
+		if rp=="repro.prog":
+			
+def ordinary_log():
+	print "ord log"
+
