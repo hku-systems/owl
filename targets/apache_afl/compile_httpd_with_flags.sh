c1bf91ece674f776c1fad1507152247f3cb1ae30
ab1gale <328149598@qq.com>
2017-09-29 16:37:27 +0800

    delete unrecognized commands & run buildconf

--- targets/apache_afl/compile_httpd_with_flags.sh
+++ targets/apache_afl/compile_httpd_with_flags.sh
@@ -26,15 +26,6 @@ echo "Running nghttp with: c-compiler:$CC $CFLAGS c++-compiler:$CXX $CXXFLAGS"
 sleep 2
 cd nghttp2* && ./configure --prefix="$PREFIX" && nghttp=$(pwd) && make clean && make -j6 && sudo make install && cd ..
 
-if [[ -z "$apr" || -z "$aprutil" || -z "$nghttp" || -z "$pcre" ]]; then
-  echo "\e[0m[-] Dependencies compilation failed."
-  echo APR: $apr
-  echo APR-Util: $aprutil
-  echo nghttp: $nghttp
-  echo PCRE8: $pcre
-  return 1;
-fi
-
 echo -e "\e[0m[+] Using the following paths"
 echo $apr
 echo $aprutil
@@ -42,5 +33,6 @@ echo $nghttp
 echo $pcre
 sleep 4
 
-cd httpd-2*   
+cd httpd-2*
+LIBS="-L$apr/.libs -L$aprutil/.libs -L$pcre/.libs -L$nghttp/lib/" CFLAGS=" $CFLAGS -I$nghttp/lib/includes -march=skylake -g -ggdb -fno-builtin -fno-inline" LDFLAGS="$CFLAGS" ./buildconf  --with-apr=$apr --with-apr-util=$aprutil
 LIBS="-L$apr/.libs -L$aprutil/.libs -L$pcre/.libs -L$nghttp/lib/" CFLAGS=" $CFLAGS -I$nghttp/lib/includes -march=skylake -g -ggdb -fno-builtin -fno-inline -fsanitize=thread" LDFLAGS="$CFLAGS" ./configure --enable-unixd --disable-pie --enable-mods-static=few --prefix="$PREFIX" --with-mpm=event --enable-http2 --with-apr=$apr --with-apr-util=$aprutil --with-nghttp2=$nghttp --enable-nghttp2-staticlib-deps --with-pcre=$pcre/pcre-config && make clean && make -j6
