fa10380102443b159596a2864693ea884641c14a
ab1gale <328149598@qq.com>
2017-10-03 19:45:56 +0800

    new report

--- targets/apache_afl/compile_httpd_with_flags.sh
+++ targets/apache_afl/compile_httpd_with_flags.sh
@@ -6,25 +6,25 @@ echo -e " \e[32mAPR"
 echo
 echo "Running apr with: c-compiler:$CC $CFLAGS c++-compiler:$CXX $CXXFLAGS"
 sleep 2
-cd apr-* && ./configure --prefix="$PREFIX" && apr=$(pwd) && make clean && make -j6 && sudo make install && cd ..
+cd apr-* && ./configure --prefix="$PREFIX" && apr=$(pwd) && make clean && make -j48 && sudo make install && cd ..
 
 echo -e " \e[33mAPRUTIL"
 echo
 echo "Running aprutil with: c-compiler:$CC $CFLAGS c++-compiler:$CXX $CXXFLAGS"
 sleep 2
-cd apr-util-* && ./configure --prefix="$PREFIX" --with-apr=$apr && aprutil=$(pwd) && make clean && make -j6 && sudo make install && cd ..
+cd apr-util-* && ./configure --prefix="$PREFIX" --with-apr=$apr && aprutil=$(pwd) && make clean && make -j48 && sudo make install && cd ..
 
 echo -e " \e[34mPCRE"
 echo
 echo "Running pcre with: c-compiler:$CC $CFLAGS c++-compiler:$CXX $CXXFLAGS"
 sleep 2
-cd pcre-8* && ./configure --disable-cpp --prefix="$PREFIX" && pcre=$(pwd) && make clean && make -j6 && sudo make install && sudo make install && cd ..
+cd pcre-8* && ./configure --disable-cpp --prefix="$PREFIX" && pcre=$(pwd) && make clean && make -j48 && sudo make install && sudo make install && cd ..
 
 echo -e " \e[35mNGHTTP"
 echo
 echo "Running nghttp with: c-compiler:$CC $CFLAGS c++-compiler:$CXX $CXXFLAGS"
 sleep 2
-cd nghttp2* && ./configure --prefix="$PREFIX" && nghttp=$(pwd) && make clean && make -j6 && sudo make install && cd ..
+cd nghttp2* && ./configure --prefix="$PREFIX" && nghttp=$(pwd) && make clean && make -j48 && sudo make install && cd ..
 
 echo -e "\e[0m[+] Using the following paths"
 echo $apr
@@ -35,4 +35,4 @@ sleep 4
 
 cd httpd-2*
 LIBS="-L$apr/.libs -L$aprutil/.libs -L$pcre/.libs -L$nghttp/lib/" CFLAGS=" $CFLAGS -I$nghttp/lib/includes -march=skylake -g -ggdb -fno-builtin -fno-inline" LDFLAGS="$CFLAGS" ./buildconf  --with-apr=$apr --with-apr-util=$aprutil
-LIBS="-L$apr/.libs -L$aprutil/.libs -L$pcre/.libs -L$nghttp/lib/" CFLAGS=" $CFLAGS -I$nghttp/lib/includes -march=skylake -g -ggdb -fno-builtin -fno-inline -fsanitize=thread" LDFLAGS="$CFLAGS" ./configure --enable-unixd --disable-pie --enable-mods-static=few --prefix="$PREFIX" --with-mpm=event --enable-http2 --with-apr=$apr --with-apr-util=$aprutil --with-nghttp2=$nghttp --enable-nghttp2-staticlib-deps --with-pcre=$pcre/pcre-config && make clean && make -j6
+LIBS="-L$apr/.libs -L$aprutil/.libs -L$pcre/.libs -L$nghttp/lib/" CFLAGS=" $CFLAGS -I$nghttp/lib/includes -march=skylake -g -ggdb -fno-builtin -fno-inline" LDFLAGS="$CFLAGS" ./configure --enable-unixd --disable-pie --enable-mods-static=few --prefix="$PREFIX" --with-mpm=event --enable-http2 --with-apr=$apr --with-apr-util=$aprutil --with-nghttp2=$nghttp --enable-nghttp2-staticlib-deps --with-pcre=$pcre/pcre-config && make clean && make -j48
