4a4a4aea1b95a9cee65ead10e135438c00c9a864
ab1gale <328149598@qq.com>
2017-10-03 19:53:12 +0800

    delete unnecessary components

--- start.py
+++ start.py
@@ -1,13 +1,4 @@
 #!/usr/bin/env python
 import os
-def start_syzkaller():
-	os.system("nohup ./fuzzers/syzkaller/bin/syz-manager -config ./fuzzers/syzkaller/my.cfg &")
 if __name__ == '__main__':
-	fp=open("owl.cfg","r")
-	line=fp.readline()
-	while line:
-		if line=="on\n":
-			on_mods=fp.readline()
-			if "kernel" in on_mods:
-				start_syzkaller()
-		line=fp.readline()
+	os.system("nohup ./fuzzers/syzkaller/bin/syz-manager -config ./fuzzers/syzkaller/my.cfg &")
